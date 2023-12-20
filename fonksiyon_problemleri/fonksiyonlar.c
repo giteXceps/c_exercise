4de40d5f232410df2ce625d35c212f9e7bd4b5d4
eXceps <sefametin68@gmail.com>
2023-12-20 22:27:40 +0300

    fonksiyonlar2

--- fonksiyon_problemleri/fonksiyonlar.c
+++ fonksiyon_problemleri/fonksiyonlar.c
@@ -1,5 +1,56 @@
 #include<stdio.h>
+//fonksiyonlar ornek ve calisma
+
+/*
+
+void hatayibas(int hata){
+    printf("Hata kodu %d",hata);
+}
 
 int main(){
-   
+    int sayi;
+    printf("Sayi giriniz");
+    scanf("%d",&sayi);
+
+    if (sayi<0){
+        hatayibas(404); // negatif girdigimizi kontrol etmek icin kullanilir
+    }
+    else{
+        printf("Girilen sayi %d\n",sayi);
+    }
+}
+
+*/
+/*
+int topla(int sayi1, int sayi2){
+    return (sayi1+sayi2);
+}
+
+int main(){
+
+    int sayi1;
+    int sayi2;
+
+    scanf("%d %d",&sayi1,&sayi2);
+    int toplam=topla(sayi1,sayi2);
+    printf("%d",toplam);
+
+}
+*/
+int fact(int sayi){
+    int fact=1;
+    for(;sayi>0;sayi--){
+        fact*=sayi;
+    }
+    return fact;
+}
+
+
+int main(){
+
+    int n;
+    printf("Sayi giriniz: ");
+    scanf("%d",&n);
+    printf("%d",fact(n));
+
 }
