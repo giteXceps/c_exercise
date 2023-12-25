a066b7959b4c3810fd2f3c31b85bb1535f9462ac
eXceps <sefametin68@gmail.com>
2023-12-25 14:26:02 +0300

    recursive modified

--- ozyinelemeli-fonksiyonlar(recursive)/recursive.c
+++ ozyinelemeli-fonksiyonlar(recursive)/recursive.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-/*
+/* 1'den 5'e kadar olan sayilarin yazdirilmasi
 void sayiYaz(int a){
     if (a==0){
         return;
@@ -14,3 +14,18 @@ int main(){
 }
 */
 
+/* 1'den 5'e kadar olan sayilarin toplamini yazdirmak
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+    else{
+        return n+sum(n-1);
+    } 
+}
+
+int main(){
+    int sonuc=sum(5);
+    printf("%d ",sonuc);
+}
+*/
