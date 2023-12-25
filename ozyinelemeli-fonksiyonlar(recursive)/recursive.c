22cb203fd14a0e2169a186015ab22597ab1c6dae
eXceps <sefametin68@gmail.com>
2023-12-25 14:07:29 +0300

    recursive functions added

---
+++ ozyinelemeli-fonksiyonlar(recursive)/recursive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+void sayiYaz(int a){
+    if (a==0){
+        return;
+    }
+    printf("%d ",a);
+    sayiYaz(a-1);
+}
+
+int main(){
+    sayiYaz(5);
+}
+*/
+
