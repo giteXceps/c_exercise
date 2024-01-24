1dd9b4fb2f9f27b387f4d356cbf5031a35f3fad7
eXceps <sefametin68@gmail.com>
2024-01-24 15:56:45 +0300

    fk

--- final_soru_ornekleri/ornekfinal.c
+++ final_soru_ornekleri/ornekfinal.c
@@ -1,27 +1,33 @@
-#include<stdio.h>
-#include<stdlib.h>
-//kullanicidan alinan bir sayinin tam bolenleri toplaminin sayinin kendisine eşit olup olmadigini yazdiriniz.
-int bolenToplami(int n){
-    int toplam=0;
-    for(int i=1;i<n;i++){
-        if(n%i==0){
-            toplam=toplam+i;
+#include <stdio.h>
+#include <stdlib.h>
+// kullanicidan alinan bir sayinin tam bolenleri toplaminin sayinin kendisine eşit olup olmadigini yazdiriniz.
+int bolenToplami(int n)
+{
+    int toplam = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            toplam = toplam + i;
         }
     }
     return toplam;
 }
-int main(){
+int main()
+{
     int sayi;
 
     printf("Sayi giriniz: ");
-    scanf("%d",&sayi);
-    int sonuc=bolenToplami(sayi);
+    scanf("%d", &sayi);
+    int sonuc = bolenToplami(sayi);
 
-    if(sonuc==sayi){
-        printf("%d sayisi tam bolenlerinin toplamina esittir.",sayi);
+    if (sonuc == sayi)
+    {
+        printf("%d sayisi tam bolenlerinin toplamina esittir.", sayi);
     }
 
-    else{
-        printf("%d sayisi tam bolenleri toplamina esit degildir.",sayi);
+    else
+    {
+        printf("%d sayisi tam bolenleri toplamina esit degildir.", sayi);
     }
 }
